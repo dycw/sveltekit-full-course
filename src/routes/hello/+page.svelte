cab206aa5feec563949d83fa84e96507bbe35f92
Derek Wan <d.wan@icloud.com>
2023-08-14 10:04:13 +0900

    dev (#2)
    * Create SvelteKit app
    
    * Finish data fetching

---
+++ src/routes/hello/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+
+  import { page } from "$app/stores";
+</script>
+
+{data.title}
+{data.text}
+{$page.data.text}
---
+++ src/routes/hello/+page.ts
@@ -0,0 +1,6 @@
+import type { PageLoad } from "./$types";
+
+export const load = (async () => {
+  const { title, text } = await fetch("someAPI").then((res) => res.json());
+  return { title, text };
+}) satisfies PageLoad;
